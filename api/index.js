0048543cd0b980eff62d0cf47be4f3be107a71e7
Kevin Roman <kevinromix@gmail.com>
2024-06-13 17:56:32 -0600

    get Cars

--- api/index.js
+++ api/index.js
@@ -1,15 +1,25 @@
 import express from "express";
+import fs from "fs";
 
 const app = express();
+
+const readData = () => {
+  try {
+    const data = fs.readFileSync("./db.json");
+    return JSON.parse(data);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("Welcome to my first API with Node js");
 });
 
 app.get("/autos", (req, res) => {
-    res.send("Welcome to my first API with Node js");
-  });
-  
+  const data = readData();
+  res.json(data.cars);
+});
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
