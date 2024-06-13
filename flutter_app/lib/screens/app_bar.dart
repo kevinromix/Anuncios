7fbada083549df8b1f47cc270a784170de209b7f
Kevin Roman <kevinromix@gmail.com>
2024-06-13 12:00:57 -0600

    Home Screen

---
+++ flutter_app/lib/screens/app_bar.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+AppBar customAppBar() {
+  return AppBar(
+    centerTitle: false,
+    // backgroundColor: Colors.amber,
+    title: const Text("Buscador Anuncio"),
+  );
+}
