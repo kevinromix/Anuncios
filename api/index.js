4e7f964f29f89fa9ddfce6f438bd0d214ffa447c
Kevin Roman <kevinromix@gmail.com>
2024-06-14 09:57:03 -0600

    send mail api method

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { json } from "express";
 import fs from "fs";
+import bodyParser from "body-parser";
+import nodemailer from "nodemailer";
+import { config } from "./config.js";
 
 const app = express();
+app.use(bodyParser.json());
 
 function sleep(ms) {
   return new Promise((resolve) => {
@@ -20,7 +24,7 @@ const readData = async (type) => {
 };
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my first API with Node js");
+  res.send("Welcome to my API with Node js");
 });
 
 // Obtener Autos
@@ -31,7 +35,49 @@ app.get("/api/autos", async (req, res) => {
   const endIndex = startIndex + pageSize;
   const data = await readData("cars");
   const autos = data.slice(startIndex, endIndex);
-  res.json(autos);
+  res.status(200).json(autos);
+});
+
+app.post("/api/mensaje", async (req, res) => {
+  const body = req.body;
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: config.mail,
+      pass: config.pass,
+    },
+  });
+
+  let nombre = body.Nombre;
+  let correo = body.Correo;
+  let mensaje = body.Mensaje;
+
+  transporter.verify().then(() => {
+    console.log("Ready for send emails");
+  });
+
+  try {
+    await transporter.sendMail({
+      from: '"Anuncios" <' + config.mail + ">",
+      to: config.mail,
+      subject: "Nuevo Mensaje",
+      html: `
+      <b>SE RECIBIO UN COMENTARIO DESDE EL SISTEMA DE ANUNCIOS EMPRESARIALES CON LA SIGUIENTE INFORMACÓN DE CONTACTO</b>
+      <br />
+      <br />
+       <div><b>NOMBRE:</b> ${nombre}</div>
+       <div><b>CORREO CONTACTO:</b> ${correo}</div>
+       <b>MENSAJE:</b>
+       <div>${mensaje}</div>
+      `,
+    });
+  } catch (er) {
+    console.log(er);
+    res.status(500).json({});
+  }
+  res.status(200).json({});
 });
 
 app.listen(3000, () => {
